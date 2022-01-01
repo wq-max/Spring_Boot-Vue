7ad287432f2484b3a71d58be369f25de010db0ba
wq-max <53320735+wq-max@users.noreply.github.com>
2022-01-01 19:06:41 +0800

    chapter1

---
+++ chapter1/demo/testspringboot/src/test/java/com/alan/test/testspringboot/TestspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.test.testspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
